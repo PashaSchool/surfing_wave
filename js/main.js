e5ee6a3055696025a0fc26aec341b89fd56e710d
Pasha School <pasha.school888@gmail.com>
2017-04-26 18:37:26 +0300

    before timeline creating

--- js/main.js
+++ js/main.js
@@ -1,22 +1,21 @@
-$(document).ready(function(){
-	var owl = $('.owl-carousel')
-  owl.owlCarousel({
-	    animateOut: 'fadeOut',
-	    items:1,
-	    loop: true,
-	    autoplay: true,
-	    autoplayTimeout: 8000
-	});
+$(document).ready(function() {
+    var owl = $('.owl-carousel')
+    owl.owlCarousel({
+        animateOut: 'fadeOut',
+        items: 1,
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 8000
+    });
 
-	$('.arrow-svg > #next').click(function(event) {
-		console.log('next');
-    	owl.trigger('next.owl.carousel');
-	});
-	
-	$('#prev').click(function(event) {
-		console.log('prev');
-    	owl.trigger('prev.owl.carousel');
-	});
+    $('.arrow-svg > #next').click(function(event) {
+        console.log('next');
+        owl.trigger('next.owl.carousel');
+    });
+
+    $('#prev').click(function(event) {
+        console.log('prev');
+        owl.trigger('prev.owl.carousel');
+    });
 
 });
-
